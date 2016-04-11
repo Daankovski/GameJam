6b5468ef4c3da795b506e501c832e5ff8648827c
Daankovski <danielepke@hotmail.com>
2016-04-11 14:10:31 +0200

    Player Movement is done

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    private Vector3 startPos;
+    private TrailRenderer playerTrail;
+    private float moveSpeed;
+
+    void Start() {
+        moveSpeed = .1f;
+        playerTrail = GameObject.Find("Player").GetComponent<TrailRenderer>();
+        startPos = GameObject.Find("startPos").transform.position;
+        playerTrail.enabled = false;
+    }
+
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            playerTrail.enabled = true;
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+        else {
+            transform.position = startPos;
+            playerTrail.enabled = false;
+        }
+        
+    }
+}
